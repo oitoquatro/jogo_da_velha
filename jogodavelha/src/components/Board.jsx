dd20a6af8f67fc85756f938dfc9f4011172838bb
Paulo Junior <paulojpjunior@gmail.com>
2024-07-31 18:16:54 -0300

    jogo da velha
    * aplicando a lógica do jogo.

--- jogodavelha/src/components/Board.jsx
+++ jogodavelha/src/components/Board.jsx
@@ -3,8 +3,13 @@ import React, { useState } from "react";
 import Square from "./Square";
 
 const Board = () => {
+  const [squares, setSquares] = useState(Array(9).fill(null));
   const [isNext, setIsNext] = useState(true);
   const winner = false;
+
+  const handleClick = (i) => {
+    console.log(i);
+  };
 
   return (
     <div>
@@ -17,19 +22,19 @@ const Board = () => {
         )}
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} onClick={() => handleClick(0)} />
+        <Square value={squares[1]} onClick={() => handleClick(1)} />
+        <Square value={squares[2]} onClick={() => handleClick(2)} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} onClick={() => handleClick(3)} />
+        <Square value={squares[4]} onClick={() => handleClick(4)} />
+        <Square value={squares[5]} onClick={() => handleClick(5)} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} onClick={() => handleClick(6)} />
+        <Square value={squares[7]} onClick={() => handleClick(7)} />
+        <Square value={squares[8]} onClick={() => handleClick(8)} />
       </div>
       <button className="reset-button">Reiniciar Jogo</button>
     </div>
--- jogodavelha/src/components/Square.jsx
+++ jogodavelha/src/components/Square.jsx
@@ -1,8 +1,13 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import React from "react";
 
-const Square = () => {
-  return <button className="square"></button>
+const Square = ({ value, onClick }) => {
+  return (
+    <button className="square" onClick={onClick}>
+      {value}
+    </button>
+  );
 };
 
 export default Square;
