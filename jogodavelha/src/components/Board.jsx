366dcc0a1cc4f5ebab9895cfe46d541b1630ea4d
Paulo Junior <paulojpjunior@gmail.com>
2024-08-01 18:48:41 -0300

    jogo da velha
    *  aplicando a lógica da AI.
    *  conclusão do projeto.

--- jogodavelha/src/components/Board.jsx
+++ jogodavelha/src/components/Board.jsx
@@ -1,15 +1,41 @@
 /* eslint-disable no-unused-vars */
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Square from "./Square";
 
 const Board = () => {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [isNext, setIsNext] = useState(true);
-  const winner = false;
+  const winner = calculateWinner(squares);
+
+  const [aiIsThinking, setAiIsThinking] = useState(false);
 
   const handleClick = (i) => {
-    console.log(i);
+    if (squares[i] || winner || aiIsThinking) return;
+
+    const newSquares = squares.slice();
+    newSquares[i] = isNext ? "X" : "O";
+
+    setSquares(newSquares);
+
+    setIsNext(!isNext);
   };
+
+  const resetGame = () => {
+    setSquares(Array(9).fill(null));
+    setIsNext(true);
+  };
+
+  useEffect(() => {
+    if(!isNext && !winner){
+      //não deixa jogar...
+      setAiIsThinking(true);
+      setTimeout(() => {
+        aiMove(squares, setSquares, setIsNext)
+        //deixa jogar...
+        setAiIsThinking(false);
+      }, 1500)
+    }
+  }, [isNext, squares, winner])
 
   return (
     <div>
@@ -36,9 +62,53 @@ const Board = () => {
         <Square value={squares[7]} onClick={() => handleClick(7)} />
         <Square value={squares[8]} onClick={() => handleClick(8)} />
       </div>
-      <button className="reset-button">Reiniciar Jogo</button>
+      <button className="reset-button" onClick={resetGame}>
+        Reiniciar Jogo
+      </button>
     </div>
   );
+};
+
+//lógica do vencedor...
+const calculateWinner = (squares) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+};
+
+//lógica da inteligência artificial...
+const aiMove = (squares, setSquares, setIsNext) => {
+  let move = null;
+
+  for (let i = 0; i < squares.length; i++) {
+    if (!squares[i]) {
+      move = i;
+      break;
+    }
+  }
+
+  if (move !== null) {
+    const newSquares = squares.slice();
+    newSquares[move] = "O";
+    setSquares(newSquares);
+    setIsNext(true);
+  }
 };
 
 export default Board;
